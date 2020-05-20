823be1abeef36bc52da5661adb2eb3076a133234
dlsrbgg33 <dlsrbgg33@gmail.com>
2020-05-21 08:23:22 +0900

    rcnn_5_21

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -110,6 +110,7 @@ class GeneralizedRCNN(nn.Module):
             return self.inference(batched_inputs)
 
         images = self.preprocess_image(batched_inputs)
+
         if "instances" in batched_inputs[0]:
             gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
         elif "targets" in batched_inputs[0]:
